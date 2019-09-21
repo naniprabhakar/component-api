0de6bc1772a55b45f28efa9bd2f745455af66746
naniprabhakar <55038229+naniprabhakar@users.noreply.github.com>
2019-09-21 15:52:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+    constructor(props){
+      super(props);
+      this.state={
+        name : " ",
+        id : " ",
+        salary : " "
+      }
+
+    }
+}
+export default App;
